388c4bc96925c24f42ffc0b7397395c968929461
Порошин Сергей <91427606+kriper2OO3@users.noreply.github.com>
2023-01-17 17:21:39 +0500

    Update Task 1.py

--- Task 1.py
+++ Task 1.py
@@ -3,7 +3,8 @@
 ### Код графика
 
 ```py
-
+import numpy as np
+import matplotlib.pyplot as plt
 
 def f(x):
     return np.sin(x) * np.log2(x)
@@ -19,7 +20,23 @@ def find_ex():
                 max_x = x[i]
     return max_x
 
+x = np.linspace(0*np.pi , 3*np.pi , 100 )
+ex_x = find_ex()
 
+# Рисуем в текущем рисованном объекте (ось x, ось y, имя нарисованной кривой, цвет линии, ширина линии)
+plt.plot(x, f(x) , label="$f(x)$", color="b", linewidth=2)
+plt.plot(ex_x, f(ex_x) ,  marker="x", color="r", linewidth=4)  #маркировка экстремума
+plt.tick_params(labelcolor='r', labelsize='medium', width=3)
+
+# X и Y координатное представление оси
+plt.xlabel("Длинна")
+plt.ylabel("Высота")
+
+# Название диаграммы
+plt.title("функция: y = sin(x) * log(x;2) и ее Экстремум")
+
+# Показать изображение
+plt.show()
 ```
 
 ###Сам график
